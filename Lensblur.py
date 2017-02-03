4f73968c1492aed19b4c7f02c2cef3bb0bb9142e
Jacob Bruce <jacobbruce2016@u.northwestern.edu>
2017-02-03 14:30:02 -0600

    Roughly final
    But with comments
    
    In case we want them?

--- Lensblur.py
+++ Lensblur.py
@@ -19,6 +19,12 @@ def grayscale(image):
     # gray[:,:,2] = averaged
     return each_layer_same(averaged, gray)
 
+def normalize(image):
+    gray = grayscale(image)
+    min_val = np.min(gray)
+    max_val = np.max(gray)
+    return (gray - min_val) * 255 / max_val
+
 def maxabs(values):
     return np.max(np.abs(values - values[4]))
 
@@ -54,7 +60,7 @@ def local_mean(image, size=5):
     # final[:,:,2] = final[:,:,0]
     return each_layer_same(scipy.ndimage.filters.convolve(image[:,:,0], weights=np.full((size,size), 1.0/(size*size))), final)
 
-def local_median(image, size=5):
+def local_median(image, size=15):
     final = np.zeros_like(image)
     # final[:,:,0] = scipy.ndimage.median_filter(image[:,:,0], (10,10))
     # final[:,:,1] = final[:,:,0]
@@ -62,15 +68,38 @@ def local_median(image, size=5):
     return each_layer_same(scipy.ndimage.median_filter(image[:,:,0], (size,size)), final)
 
 def threshold(image, percent):
-    threshold = (np.max(image) - np.min(image)) * percent + np.min(image)
-    image[image < threshold] = 0
-    image[image >= threshold] = 255
-    return image
+    threshold = (np.max(image) - np.min(image)) * (percent / 100.) + np.min(image)
+    new_image = np.zeros_like(image)
+    new_image[image < threshold] = 0
+    new_image[image >= threshold] = 255
+    return new_image
 
+def laplace(image):
+    final = np.zeros_like(image)
+    return each_layer_same(scipy.ndimage.filters.laplace(image[:,:,0]), final)
+
+def sharpen(image):
+    final = np.zeros_like(image)
+    # weights1 = np.array([[-.1, -.1, -.1],
+    #                     [ -.1, 1.9, -.1],
+    #                     [-.1, -.1, -.1]])
+    one_layer = scipy.misc.imfilter(image[:,:,0], ftype='sharpen')
+    # one_layer = scipy.misc.imfilter(one_layer, ftype='sharpen')
+    # one_layer = scipy.misc.imfilter(one_layer, ftype='sharpen')
+    # one_layer = scipy.misc.imfilter(one_layer, ftype='sharpen')
+    # one_layer = scipy.misc.imfilter(one_layer, ftype='sharpen')
+    return each_layer_same(one_layer, final)
 # filepath depends on who's running the code
 
+def NAND(image1, image2):
+    final = np.zeros_like(image1)
+    final[ image1 == 0 ] = 255
+    final[ image2 == 0 ] = 255
+    # final = np.bitwise_and(image1, image2)
+    return final
+
 filepath_will = "c:/Users/Will Molter/Pictures/Geospatial vision proj1/sample_drive/cam_3/"
-filepath_jacob = "/Users/jdbruce/Downloads/WQ2017/Geospatial/sample_drive/cam_3/"
+filepath_jacob = "/Users/jdbruce/Downloads/WQ2017/Geospatial/sample_drive/cam_0/"
 
 filepath = filepath_jacob # change this line when you change users
 
@@ -78,67 +107,111 @@ filepath = filepath_jacob # change this line when you change users
 # loop portion
 # comment here so it doesn't loop
 
-
-# num_images = 1000
-# start_image = 100
-
-# paths = [filepath + filename for filename in os.listdir(filepath)[start_image: start_image + num_images]]
-# prev_image = imageio.imread(paths[0])
-
-# counter = 0
-# for filename in paths[0:]:
-#     im = imageio.imread(filename)
-#     if counter == 0:
-#         running_total = np.zeros(imageio.imread(filename).shape)
-#         subtract_total = np.zeros_like(running_total)
-#         gradient_running_total = np.zeros_like(running_total)
-#     running_total += im
-#     # gradient_running_total += get_true_gradient(im)
-#     print counter
-#     counter += 1
-#     subtract_total += np.abs(im - prev_image)
-#     prev_image = im
+    def folder_mask(path):
 
 
+    num_images = 10
+    start_image = 100
 
-# subtract_total_gray = grayscale(subtract_total)
-# subtract_normalized = threshold(subtract_total_gray, .6)
-# average = running_total / num_images
-# print np.min(subtract_normalized)
+    paths = [filepath + filename for filename in os.listdir(filepath)[start_image: start_image + num_images]]
+    prev_image = imageio.imread(paths[0])
 
-# imageio.imwrite("subtract_gray.jpg", subtract_total_gray)
-# imageio.imwrite("subtraction.jpg", subtract_normalized)
-# gradient_running_total = imageio.imread("gradient.jpg")
+    counter = 0
+    for filename in paths[0:]:
+        im = imageio.imread(filename)
+        if counter == 0:
+            running_total = np.zeros(imageio.imread(filename).shape)
+            subtract_total = np.zeros_like(running_total)
+            gradient_running_total = np.zeros_like(running_total)
+            running_average = np.zeros_like(running_total)
+            total_approx_dev = np.zeros_like(running_total)
+        running_total += im
+        running_average = (im + running_average * counter) / (counter + 1)
+        total_approx_dev += np.abs(im - running_average)
+        gradient_running_total += prewitt_gradient(im)
+        print counter
+        counter += 1
+        subtract_total += np.abs(im - prev_image)
+        prev_image = im
 
-average = imageio.imread("average.jpg")
-# average_sobel = sobel_gradient(average)
-average_prewitt = imageio.imread("average_prewitt.jpg")
-# average_grad = get_true_gradient(average)
-# average_prewitt = get_prewitt_gradient(average)
-average_sobel = imageio.imread("average_sobel.jpg")
+    # get mask 1 (from grad)
 
-average_prewitt_mean = local_mean(average_prewitt, 10)
-# average_prewitt_mean_mean = local_mean(average_prewitt_mean)
-average_prewitt_median_5 = local_median(average_prewitt, 5)
-average_prewitt_median_10 = local_median(average_prewitt, 10)
-average_prewitt_median_15 = local_median(average_prewitt, 15)
-average_prewitt_median_20 = local_median(average_prewitt, 20)
+    grad_norm = normalize(gradient_running_total)
+    imageio.imwrite("grad_norm.jpg", grad_norm)
+    grad_norm_threshold = threshold(grad_norm, 40)
+    imageio.imwrite("grad_norm_threshold.jpg", grad_norm_threshold)
+    grad_norm_threshold_median = local_median(grad_norm_threshold)
+    imageio.imwrite("grad_norm_threshold_median.jpg", grad_norm_threshold_median)
 
-imageio.imwrite("average.jpg", average)
-imageio.imwrite("average_prewitt.jpg", average_prewitt)
-imageio.imwrite("average_sobel.jpg", average_sobel)
-imageio.imwrite("average_prewitt_mean.jpg", average_prewitt_mean)
-imageio.imwrite("average_prewitt_median_5.jpg", average_prewitt_median_5)
-imageio.imwrite("average_prewitt_median_10.jpg", average_prewitt_median_10)
-imageio.imwrite("average_prewitt_median_15.jpg", average_prewitt_median_15)
-imageio.imwrite("average_prewitt_median_20.jpg", average_prewitt_median_20)
-# imageio.imwrite("average_prewitt_mean_mean.jpg", average_prewitt_mean_mean)
+    mask_1_final = grad_norm_threshold_median
 
-# imageio.imwrite("gradient.jpg", gradient_running_total)
-# imageio.imwrite("gradient_threshold_median.jpg", local_median(threshold(gradient_running_total, .4)))
+    # get mask 2 (from dev)
 
+    dev_normalized = normalize(total_approx_dev)
+    imageio.imwrite("deviation.jpg", dev_normalized)
 
+    # dev_normalized = imageio.imread("deviation.jpg")
+    # imageio.imwrite("dev_normalized.jpg", dev_normalized)
+    # # dev_normalized_mean = local_mean(dev_normalized, 50)
+    dev_normalized_median =  local_median(dev_normalized)  #imageio.imread("dev_normalized_median.jpg") #
+    # dev_normalized_median_median = local_median(dev_normalized_median, 20)  #
+    # imageio.imwrite("dev_normalized_median_median.jpg", dev_normalized_median_median)
+    imageio.imwrite("dev_normalized_median.jpg", dev_normalized_median)
+
+    # dev_normalized_median_prewitt = prewitt_gradient(dev_normalized_median)
+    # imageio.imwrite("dev_normalized_median_prewitt.jpg", dev_normalized_median_prewitt)
 
 
+    # dev_normalized_median_sharpen = sharpen(dev_normalized_median)
+    # imageio.imwrite("dev_normalized_median_sharpen.jpg", dev_normalized_median_sharpen)
 
 
+    #
+    dev_normalized_median_threshold = threshold(dev_normalized_median, 10)
+    imageio.imwrite("dev_normalized_median_threshold.jpg", dev_normalized_median_threshold)
+
+    mask_2_final = dev_normalized_median_threshold
+
+    # final output_array
+
+    # mask_1_final = imageio.imread("grad_norm_threshold_median.jpg")
+    # mask_2_final = imageio.imread("dev_normalized_median_threshold.jpg")
+
+    final_mask = NAND(mask_1_final, mask_2_final)
+    imageio.imwrite("final_mask.jpg", final_mask)
+
+
+
+
+
+    # dev_normalized_median_gradient = true_gradient(dev_normalized_median)
+    # imageio.imwrite("dev_normalized_median_gradient.jpg", dev_normalized_median_gradient)
+
+    # dev_normalized_prewitt = prewitt_gradient(dev_normalized)
+    # imageio.imwrite("dev_normalized_prewitt.jpg",dev_normalized_prewitt)
+
+    # average = imageio.imread("average.jpg")
+    # # average_sobel = sobel_gradient(average)
+    # average_prewitt = imageio.imread("average_prewitt.jpg")
+    # # average_grad = get_true_gradient(average)
+    # # average_prewitt = get_prewitt_gradient(average)
+    # average_sobel = imageio.imread("average_sobel.jpg")
+    #
+    # average_prewitt_mean = local_mean(average_prewitt, 10)
+    # # average_prewitt_mean_mean = local_mean(average_prewitt_mean)
+    # average_prewitt_median_5 = local_median(average_prewitt, 5)
+    # average_prewitt_median_10 = local_median(average_prewitt, 10)
+    # average_prewitt_median_15 = local_median(average_prewitt, 15)
+    # average_prewitt_median_20 = local_median(average_prewitt, 20)
+    #
+    # imageio.imwrite("average.jpg", average)
+    # imageio.imwrite("average_prewitt.jpg", average_prewitt)
+    # imageio.imwrite("average_sobel.jpg", average_sobel)
+    # imageio.imwrite("average_prewitt_mean.jpg", average_prewitt_mean)
+    # imageio.imwrite("average_prewitt_median_5.jpg", average_prewitt_median_5)
+    # imageio.imwrite("average_prewitt_median_10.jpg", average_prewitt_median_10)
+    # imageio.imwrite("average_prewitt_median_15.jpg", average_prewitt_median_15)
+    # imageio.imwrite("average_prewitt_median_20.jpg", average_prewitt_median_20)
+    # imageio.imwrite("average_prewitt_mean_mean.jpg", average_prewitt_mean_mean)
+
+    # imageio.imwrite("gradient.jpg", gradient_running_total)

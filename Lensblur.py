334bcb4a2c995fdb01b84f3a92a37c4f67aedd83
Jacob Bruce <jacobbruce2016@u.northwestern.edu>
2017-02-04 17:35:08 -0600

    w/ command line input
    And good comments and such

--- Lensblur.py
+++ Lensblur.py
@@ -2,33 +2,50 @@ import imageio
 import os
 import numpy as np
 import scipy.ndimage
+import sys
+
+# Automatic Lens Smear Detection
+# Geospatial Vision and Visualization
+# WQ 2017 HW1
+# Will Molter and Jacob Bruce
+#
+# To run from the command line, provide the path to the
+# sample_drive directory (the expanded .rar) which holds
+# the folders cam_0, 1, 2, 3, and 5.
+# Expected running time is around 2 hours per folder
+# Example: python Lensblur.py /Users/jdbruce/Downloads/WQ2017/Geospatial/sample_drive/
 
 # helper functions
 
 def each_layer_same(flat_array, output_array):
+    # takes 2D array of values and copies it into each layer of a 3D array
+    # useful for performing grayscale operations on one layer and then replacing it
     output_array[:,:,0] = flat_array
     output_array[:,:,1] = flat_array
     output_array[:,:,2] = flat_array
     return output_array
 
 def grayscale(image):
+    # gives the grayscale of the image by averaging across RGB
     averaged = np.mean(image, 2)
     gray = np.zeros_like(image)
-    # gray[:,:,0] = averaged
-    # gray[:,:,1] = averaged
-    # gray[:,:,2] = averaged
     return each_layer_same(averaged, gray)
 
 def normalize(image):
+    # normalizes a 3D array to be written as an image
+    # stretches it to put its minimum value at 0 and its maximum at xFF
     gray = grayscale(image)
     min_val = np.min(gray)
     max_val = np.max(gray)
     return (gray - min_val) * 255 / max_val
 
 def maxabs(values):
+    # gives the maximum difference between the center value and any of the others
+    # useful for calculating the gradient
     return np.max(np.abs(values - values[4]))
 
 def true_gradient(image):
+    # calculates the gradient of an image by calculating the maximum change at each pixel
     footprint = np.ones((3,3))
     result = np.zeros_like(image)
     result[:,:,0] = scipy.ndimage.filters.generic_filter(image[:,:,0], maxabs, footprint=footprint)
@@ -37,6 +54,7 @@ def true_gradient(image):
     return result
 
 def sobel_gradient(image):
+    # wrapper function for scipy's sobel gradient to run on images
     image = grayscale(image)
     result = np.zeros_like(image)
     result = scipy.ndimage.filters.sobel(image, axis=1)
@@ -44,30 +62,27 @@ def sobel_gradient(image):
     return result
 
 def prewitt_gradient(image):
+    # wrapper function for scipy's prewitt gradient to run on images
     image = grayscale(image)
-    # footprint = np.ones((3,3))
     result = np.zeros_like(image)
     result= scipy.ndimage.prewitt(image, axis=1)
     result += scipy.ndimage.prewitt(image, axis=0)
-    # result[:,:,1] = scipy.ndimage.filters.prewitt(image[:,:,1])
-    # result[:,:,2] = scipy.ndimage.filters.prewitt(image[:,:,2])
     return result
 
 def local_mean(image, size=5):
+    # caclulates the local mean for a pixel within a neighborhood of a given size
     final = np.zeros_like(image)
-    # final[:,:,0] = scipy.ndimage.filters.convolve(image[:,:,0], weights=np.full((5,5), 1.0/25))
-    # final[:,:,1] = final[:,:,0]
-    # final[:,:,2] = final[:,:,0]
     return each_layer_same(scipy.ndimage.filters.convolve(image[:,:,0], weights=np.full((size,size), 1.0/(size*size))), final)
 
 def local_median(image, size=15):
+    # caclulates the local median for a pixel within a neighborhood of a given size
     final = np.zeros_like(image)
-    # final[:,:,0] = scipy.ndimage.median_filter(image[:,:,0], (10,10))
-    # final[:,:,1] = final[:,:,0]
-    # final[:,:,2] = final[:,:,0]
     return each_layer_same(scipy.ndimage.median_filter(image[:,:,0], (size,size)), final)
 
 def threshold(image, percent):
+    # turns the image into a black and white image by setting a percent of the image range
+    # above which pixels are only white and below which pixels are only black
+    # should only be used on grayscale images
     threshold = (np.max(image) - np.min(image)) * (percent / 100.) + np.min(image)
     new_image = np.zeros_like(image)
     new_image[image < threshold] = 0
@@ -75,64 +90,57 @@ def threshold(image, percent):
     return new_image
 
 def laplace(image):
+    # wrapper function for scipy's laplace operator to run on images
     final = np.zeros_like(image)
     return each_layer_same(scipy.ndimage.filters.laplace(image[:,:,0]), final)
 
 def sharpen(image):
+    # wrapper function for scipy's sharpen operator to run on images
     final = np.zeros_like(image)
-    # weights1 = np.array([[-.1, -.1, -.1],
-    #                     [ -.1, 1.9, -.1],
-    #                     [-.1, -.1, -.1]])
     one_layer = scipy.misc.imfilter(image[:,:,0], ftype='sharpen')
-    # one_layer = scipy.misc.imfilter(one_layer, ftype='sharpen')
-    # one_layer = scipy.misc.imfilter(one_layer, ftype='sharpen')
-    # one_layer = scipy.misc.imfilter(one_layer, ftype='sharpen')
-    # one_layer = scipy.misc.imfilter(one_layer, ftype='sharpen')
     return each_layer_same(one_layer, final)
-# filepath depends on who's running the code
 
 def NAND(image1, image2):
+    # takes two inverted masks and returns their NAND
+    # assuming all inputs are already thresholded to 0 and xFF
     final = np.zeros_like(image1)
     final[ image1 == 0 ] = 255
     final[ image2 == 0 ] = 255
-    # final = np.bitwise_and(image1, image2)
     return final
 
-filepath_will = "c:/Users/Will Molter/Pictures/Geospatial vision proj1/sample_drive/cam_3/"
-filepath_jacob = "/Users/jdbruce/Downloads/WQ2017/Geospatial/sample_drive/"
-
-filepath = filepath_jacob # change this line when you change users
+# body functions
 
 def run_all(sample_drive_path):
+    # runs folder_mask on each subfolder in the given path
     folders = os.listdir(sample_drive_path)
     print folders
     for folder in folders:
         if folder[:3] == "cam":
             folder_mask(sample_drive_path+folder+"/", folder)
 
-
 def folder_mask(input_path, output_path):
+    # computes the lens smear mask for the set of pictures in a given folder
+    # see slides for description of methodology
     print "Running folder: "
     print input_path
     print "Storing output in: "
     print output_path
-    # num_images = 5
-    # start_image = 100
 
-    paths = [input_path + filename for filename in os.listdir(input_path)]#[start_image: start_image + num_images]]
+    paths = [input_path + filename for filename in os.listdir(input_path)]
     prev_image = imageio.imread(paths[0])
 
     counter = 0
     for filename in paths[0:]:
         im = imageio.imread(filename)
         if counter == 0:
+            # array initialization
             running_total = np.zeros(imageio.imread(filename).shape)
             subtract_total = np.zeros_like(running_total)
             gradient_running_total = np.zeros_like(running_total)
             running_average = np.zeros_like(running_total)
             total_approx_dev = np.zeros_like(running_total)
         running_total += im
-        running_average = (im + running_average * counter) / (counter + 1)
+        running_average = (im + running_average * counter) / (counter + 1) # recompute running average
         total_approx_dev += np.abs(im - running_average)
         gradient_running_total += prewitt_gradient(im)
         print counter
@@ -171,4 +179,7 @@ def folder_mask(input_path, output_path):
     imageio.imwrite(output_path + "final_mask.jpg", final_mask)
     return
 
-run_all(filepath_jacob)
+# runs folder iteration function
+# based on command line input
+
+run_all(sys.argv[1])

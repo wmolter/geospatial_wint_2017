a66865f38e5bd266ce8d8dc49ae48c3ff514eb2a
Will Molter <williammolter2018@u.northwestern.edu>
2017-02-10 14:41:54 -0600

    visualization for QGIS

--- map_matching.py
+++ map_matching.py
@@ -15,7 +15,7 @@ class ProbePoint:
         direction = None        # is the direction the vehicle was travelling on thelink (F = from ref node, T = towards ref node).
         distFromRef = None      # is the distance from the reference node to the map-matched probe point location on the link in decimal meters.
         distFromLink = None       # is the perpendicular distance from the map-matched probe point location on the link to the probe point in decimal meters.
-
+        matchedLinke = None
 
 class Link:
     def __init__(self, list_in):
@@ -79,16 +79,17 @@ def parse_links_csv(path):
 
 # helper functions
 
-def assign_nodes(path):
+def assign_nodes(nodepath, linkpath):
     print "Parsing links"
-    links_list = parse_links_csv(jacob_path + links_filename)
+    links_list = parse_links_csv(linkpath)
     print "Done parsing links"
-    with open(path, 'rb') as probe_points_file:
+    with open(nodepath, 'rb') as probe_points_file:
         probe_point_line_reader = csv.reader(probe_points_file)
 
         counter = 0
         decimation = 100
         counter_stop = 1500
+        probe_list = []
         for probe_point_line in probe_point_line_reader:
             if counter % decimation != 0:
                 counter = counter + 1
@@ -96,6 +97,7 @@ def assign_nodes(path):
             # print probe_point_line
             print counter
             probe_point = ProbePoint(probe_point_line)
+            probe_list.append(probe_point)
             best_distance = float("inf")
             for link in links_list:
                 this_distance = probe_point.point3D.distance_2D(link.refNode)
@@ -103,19 +105,41 @@ def assign_nodes(path):
                     best_distance = this_distance
                     best_link = link
             probe_point.linkPVID = best_link.linkPVID
+            probe_point.matchedLink = best_link
             print "For sampleID: ", probe_point.sampleID
             print "We matched link: ", probe_point.linkPVID
 
             counter = counter + 1
             if counter > counter_stop:
                 break
+    return probe_list
+
+def write_links_csv(links, filename):
+    out_file = open(filename, 'wb')
+    writer = csv.writer(out_file, delimiter = ",")
+    for link in links:
+        for control_point in link.shapeInfo:
+            line = [link.linkPVID, control_point.latitude, control_point.longitude]
+            writer.writerow(line)
+    out_file.close()
+
+def write_probe_csv(probe_points, filename):
+    out_file = open(filename, 'wb')
+    writer = csv.writer(out_file, delimiter=",")
+    for probe in probe_points:
+        line = [probe.sampleID, probe.point3D.latitude, probe.point3D.longitude]
+        writer.writerow(line)
+    out_file.close()
 
 
 
 jacob_path = '/Users/jdbruce/Downloads/WQ2017/Geospatial/probe_data_map_matching/'
+will_path = 'c:/Users/Will Molter/Documents/College/Winter 2017/EECS 395/proj2/'
 links_filename = 'Partition6467LinkData.csv'
 points_filename = 'Partition6467ProbePoints.csv'
 
-assign_nodes(jacob_path + points_filename)
+probe_list = assign_nodes(will_path + points_filename, will_path + links_filename)
+write_probe_csv(probe_list, "probes.csv")
+write_links_csv([probe.matchedLink for probe in probe_list], "links.csv")
 
 # parse_point_from_string('51.4965800/9.3862299/|51.4994700/9.3848799/')
